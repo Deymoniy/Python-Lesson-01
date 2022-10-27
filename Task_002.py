46010de807b571949388df434b5c5afa71e1ea6c
Yegor L <mmarf@inbox.ru>
2022-10-27 17:40:19 +0600

    done

--- Task_002.py
+++ Task_002.py
@@ -4,3 +4,16 @@
 #Пример:
 #- [2, 3, 4, 5, 6] => [12, 15, 16];
 #- [2, 3, 5, 6] => [12, 15]
+ints = [x for x in range(1, 11)]
+first=[]
+last= []
+res = []
+item = 0
+for item in range(len(ints) // 2):
+    first.append(ints[item])  
+ints.reverse()
+for item in range(len(ints) // 2):
+    last.append(ints[item])
+for item in range(len(first)):
+    res.append(first[item] * last[item])   
+print(res)
